395d18c1c5d0974c82b59891b991566a7d05d4cc
JerryChi <ttt210200048@gmail.com>
2018-02-26 23:30:53 +0800

    seague & new controllers

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,21 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgimg: UIImageView!
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwingSegue: UIStoryboardSegue){
+        
     }
 
-
 }
-
